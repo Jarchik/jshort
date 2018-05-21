606de8d55c08f8648f47ef268d8bc5f981e16249
yaroslav.lvivskyi <yaroslav.lvivskyi@eleks.com>
2018-05-21 20:18:40 +0300

    Initial commit

---
+++ app/models/Link.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yaroslav.lvivskyi
+ * Date: 5/21/2018
+ * Time: 7:23 PM
+ */
+class Link extends Eloquent {
+    protected $fillable = array('url', 'code');
+}
